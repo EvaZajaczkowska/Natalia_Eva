3350f6344b3793ef7c1a56de2db93e7ce45d3585
eva anna <eva.zajaczkowska@gmail.com>
2020-09-23 11:26:50 +0200

    test

---
+++ basic.js
@@ -0,0 +1,12 @@
+function userdata() {
+	var select = document.getElementbyId("input").innerHTML;
+	console.log("input");
+
+}
+document.getElementById('submit').addEventListener("click", firstName, lastName, age, false);
+
+
+
+function submitData() {
+	
+}
